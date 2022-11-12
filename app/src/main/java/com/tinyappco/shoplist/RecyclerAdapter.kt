d522a686630654188056664f92c2b0870f54ed85
Andrew Muncey <a.muncey@chester.ac.uk>
2022-11-12 13:47:27 +0000

    implement strikethrough

--- app/src/main/java/com/tinyappco/shoplist/RecyclerAdapter.kt
+++ app/src/main/java/com/tinyappco/shoplist/RecyclerAdapter.kt
@@ -8,6 +8,14 @@ import com.tinyappco.shoplist.databinding.CardLayoutBinding
 
 class RecyclerAdapter : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
 
+    var list = mutableListOf<ShoppingListItem>()
+
+    init{
+        list.add(ShoppingListItem("bread",2))
+        val cheese = ShoppingListItem("cheese",1)
+        cheese.purchased = true
+        list.add(cheese)
+    }
 
     inner class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
         val binding = CardLayoutBinding.bind(itemView)
@@ -19,11 +27,19 @@ class RecyclerAdapter : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.binding.tvCount.text = position.toString()
-        holder.binding.tvProduct.text = if (position == 1) "Banana" else "Bananas"
+        val cardView = holder.binding
+        val item = list[position]
+        cardView.tvCount.text = item.count.toString()
+        cardView.tvProduct.text = item.name
+
+        if (item.purchased){
+            cardView.tvProduct.toggleStrikeThrough(true)
+            cardView.tvCount.toggleStrikeThrough(true)
+        }
+
     }
 
     override fun getItemCount(): Int {
-        return 125
+        return list.size
     }
 }
