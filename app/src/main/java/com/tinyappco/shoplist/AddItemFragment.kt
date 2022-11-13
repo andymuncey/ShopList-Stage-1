bccabe300e7bb5d25a492a1f4350535f69cff52a
Andrew Muncey <a.muncey@chester.ac.uk>
2022-11-13 16:14:29 +0000

    clear redundant code

--- app/src/main/java/com/tinyappco/shoplist/AddItemFragment.kt
+++ app/src/main/java/com/tinyappco/shoplist/AddItemFragment.kt
@@ -1,8 +1,6 @@
 package com.tinyappco.shoplist
 
-import android.app.Activity
 import android.content.Context
-import android.content.Intent
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
@@ -10,21 +8,7 @@ import android.view.inputmethod.EditorInfo
 import android.widget.TextView
 import com.tinyappco.shoplist.databinding.FragmentAddItemBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-//private const val ARG_PARAM1 = "param1"
-//private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [AddItemFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AddItemFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-//    private var param1: String? = null
-//    private var param2: String? = null
-
 
     interface AddItemFragmentListener {
         fun onItemAdded(item: ShoppingListItem)
@@ -34,7 +18,6 @@ class AddItemFragment : Fragment() {
 
     private lateinit var binding : FragmentAddItemBinding
 
-
     override fun onAttach(context: Context) {
         super.onAttach(context)
 
@@ -43,49 +26,20 @@ class AddItemFragment : Fragment() {
         }
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-//        arguments?.let {
-//            param1 = it.getString(ARG_PARAM1)
-//            param2 = it.getString(ARG_PARAM2)
-//        }
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-
         binding = FragmentAddItemBinding.inflate(inflater)
         return binding.root
-
     }
-
-//    companion object {
-//        /**
-//         * Use this factory method to create a new instance of
-//         * this fragment using the provided parameters.
-//         *
-//         * @param param1 Parameter 1.
-//         * @param param2 Parameter 2.
-//         * @return A new instance of fragment AddItemFragment.
-//         */
-//        // TODO: Rename and change types and number of parameters
-//        @JvmStatic
-//        fun newInstance(param1: String, param2: String) =
-//            AddItemFragment().apply {
-//                arguments = Bundle().apply {
-//                    putString(ARG_PARAM1, param1)
-//                    putString(ARG_PARAM2, param2)
-//                }
-//            }
-//    }
 
 
     private fun validProductName() : Boolean {
         return binding.etItem.text.length > 0
     }
+
     private fun productCount() : Int {
         val userCount = binding.etCount.text.toString().toIntOrNull()
         return if (userCount == null) 1 else userCount
@@ -106,7 +60,6 @@ class AddItemFragment : Fragment() {
         //display keyboard
         activity?.window?.setSoftInputMode(
             WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE)
-
     }
 
 
@@ -126,18 +79,11 @@ class AddItemFragment : Fragment() {
                 if (validProductName()) {
                     val product = ShoppingListItem(binding.etItem.text.toString(),productCount())
                     addItemListener?.onItemAdded(product)
-
-//                    val intent = Intent()
-//                    intent.putExtra("item", product)
-//                    activity?.setResult(Activity.RESULT_OK, intent)
-//                    activity?.finish()
-//we have consumed (handled) this event (key press) return true
+            //we have consumed (handled) this event (key press) return true
                 }
             }
-//we have not consumed this event (i.e. different key pressed or no valid pr oduct entered yet
+            //we have not consumed this event (i.e. different key pressed or no valid product entered yet
             return false
-
-
         }
     }
 
