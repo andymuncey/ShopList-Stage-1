faf750beb9b3157ddaf291dd78d70a2a0159cfd1
Andrew Muncey <a.muncey@chester.ac.uk>
2022-11-17 10:10:06 +0000

    migrate recyclerview to fragment

---
+++ app/src/main/java/com/tinyappco/shoplist/ShopListFragment.kt
@@ -0,0 +1,126 @@
+package com.tinyappco.shoplist
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.tinyappco.shoplist.databinding.FragmentAddItemBinding
+import com.tinyappco.shoplist.databinding.FragmentShopListBinding
+import java.io.ObjectInputStream
+import java.io.ObjectOutputStream
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [ShopListFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class ShopListFragment : Fragment() {
+
+
+    private lateinit var layoutManager: RecyclerView.LayoutManager
+
+    lateinit var adapter: RecyclerAdapter
+
+    private lateinit var binding: FragmentShopListBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    private fun loadList() {
+        try {
+            val fileInputStream = activity?.openFileInput("list.dat")
+            val objectInputStream = ObjectInputStream(fileInputStream)
+
+            @Suppress("UNCHECKED_CAST")
+            val list = objectInputStream.readObject() as? MutableList<ShoppingListItem>
+            if (list != null) {
+                adapter.list = list
+            }
+            objectInputStream.close()
+            fileInputStream?.close()
+        } catch (e: java.io.FileNotFoundException) {
+            //loading has failed, probably first run
+            Toast.makeText(activity, "No existing list found", Toast.LENGTH_LONG).show()
+        }
+    }
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentShopListBinding.inflate(inflater)
+
+
+
+
+        return binding.root
+    }
+
+
+
+    private fun saveList(){
+        val fileOutputStream = activity?.openFileOutput("list.dat", Context.MODE_PRIVATE)
+        val objectOutputStream = ObjectOutputStream(fileOutputStream)
+        objectOutputStream.writeObject(adapter.list)
+        objectOutputStream.close()
+        fileOutputStream?.close()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        saveList()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        saveList()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        layoutManager = LinearLayoutManager(activity)
+
+        binding.rvShoppingList.layoutManager = layoutManager
+        adapter = RecyclerAdapter()
+        binding.rvShoppingList.adapter = adapter
+
+        handleDragging()
+
+        loadList()
+    }
+
+    private fun handleDragging(){
+        val dragCallback = DragCallback()
+        val touchHelper = ItemTouchHelper(dragCallback)
+        touchHelper.attachToRecyclerView(binding.rvShoppingList)
+    }
+
+    inner class DragCallback : ItemTouchHelper.Callback() {
+        override fun getMovementFlags(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder): Int {
+            val dragFlags = ItemTouchHelper.UP or ItemTouchHelper.DOWN
+            return makeMovementFlags(dragFlags, 0)
+
+        }
+        override fun onMove(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, target: RecyclerView.ViewHolder): Boolean {
+
+            adapter.notifyItemMoved(viewHolder.bindingAdapterPosition,target.bindingAdapterPosition)
+            return true
+        }
+        override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+        }
+    }
+
+
+}
